6948e1686534ab203b9b66cf1abcb35a637e3558
Raitoxz <2560804242@qq.com>
2016-03-27 20:37:51 +0800

    赵师傅说用bitset，于是试了一下，在leetcode上面跑的时间比我的解法要快很多。

--- Reverse_Bits.cpp
+++ Reverse_Bits.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
+#include<bitset>
 
 using namespace std;
 
 int main(){
-    uint32_t n = 00000000000000000000000000000001;
-    uint32_t result = 00000000000000000000000000000000;
+    uint32_t n = 0x0001;
+    uint32_t result = 0x0000;
     for(int i = 0; i < 32; i++){
         //错误的代码,错误的原因是没有考虑优先级，<<、&的优先级都很低。。。。
         //result = result << 1 + n&1;
@@ -13,5 +14,13 @@ int main(){
         n = n >> 1;
     }
     cout << result << endl;
+
+    //bitset实现
+    bitset<32> b;
+    for(int i = 31; i >= 0; i--){
+        b[i] = n&1;
+        n = n >> 1;
+    }
+    cout << b.to_ulong() << endl;
     return 0;
 }
